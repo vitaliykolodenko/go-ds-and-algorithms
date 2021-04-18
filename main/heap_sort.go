2de0b2abcea6b612cfc3ea9c09c8be1a3d2659d0
Vitalii Kolodenko <vitalii.kolodenko@paytm.com>
2021-04-18 00:12:09 -0400

    - add heapify - add comments -export Heap struct

--- main/heap_sort.go
+++ main/heap_sort.go
@@ -1,21 +1,22 @@
 package main
 
-import "fmt"
-
-type heap struct {
+//Heap store elements and it's length for heap ds
+type Heap struct {
 	data   []int
 	length int
 }
 
-func NewHeap(size int) *heap {
-	h := heap{
+// NewHeap creates new Heap with predefined capacity
+func NewHeap(size int) *Heap {
+	h := Heap{
 		data:   make([]int, 0, size),
 		length: 0,
 	}
 	return &h
 }
 
-func (h *heap) Add(item int) {
+//Add adds and item to the Heap
+func (h *Heap) Add(item int) {
 	h.data = append(h.data, item)
 	h.length++
 	i := h.length - 1
@@ -23,17 +24,31 @@ func (h *heap) Add(item int) {
 		h.data[i], h.data[i/2] = h.data[i/2], h.data[i]
 		i /= 2
 	}
-
-	fmt.Printf("Heap after add: %v \n", h.data)
 }
 
-func (h *heap) Max() int {
+//Max gets current max in the Heap
+func (h *Heap) Max() int {
 	return h.data[0]
 }
 
-func (h *heap) ExtractMax() int {
+//ExtractMax returns current max and removes it from the Heap
+func (h *Heap) ExtractMax() int {
 	max := h.data[0]
 	h.data[0], h.data[h.length-1] = h.data[h.length-1], h.data[0]
 	h.length--
+	heapify(h, 0)
 	return max
+}
+
+func heapify(h *Heap, n int) {
+	l := (n+1)*2 - 1
+	r := (n + 1) * 2
+
+	if l < h.length && h.data[n] < h.data[l] {
+		h.data[n], h.data[l] = h.data[l], h.data[n]
+		heapify(h, l)
+	} else if r < h.length && h.data[n] < h.data[r] {
+		h.data[n], h.data[r] = h.data[r], h.data[n]
+		heapify(h, r)
+	}
 }
--- main/heap_sort_test.go
+++ main/heap_sort_test.go
@@ -6,11 +6,11 @@ func TestCreation(t *testing.T) {
 	h := NewHeap(3)
 
 	if h.length != 0 {
-		t.Fatalf("Newly created heap is not 0 size, size: %d", h.length)
+		t.Fatalf("Newly created Heap is not 0 size, size: %d", h.length)
 	}
 
 	if len(h.data) != 0 {
-		t.Fatalf("Newly created heap has some items, len: %d", len(h.data))
+		t.Fatalf("Newly created Heap has some items, len: %d", len(h.data))
 	}
 }
 
@@ -37,5 +37,29 @@ func TestHeapMax(t *testing.T) {
 
 	if max != 5 {
 		t.Fatalf("Heap did not return max, returned: %d", max)
+	}
+}
+
+func TestHeapExtractMax(t *testing.T) {
+	h := NewHeap(3)
+
+	h.Add(1)
+	h.Add(2)
+	h.Add(5)
+
+	max := h.ExtractMax()
+
+	if max != 5 {
+		t.Fatalf("Heap did not return max, returned: %d", max)
+	}
+
+	max = h.Max()
+
+	if max != 2 {
+		t.Fatalf("Heap did not extract previous max, returned: %d", max)
+	}
+
+	if h.length != 2 {
+		t.Fatalf("Heap did not extract previous max, length still: %d", h.length)
 	}
 }
